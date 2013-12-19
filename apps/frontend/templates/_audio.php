48bed6dd7725a0f2dd8ccc6b735824e2f462bfd0
Luxin YAO <luxin.yao@hotmail.com>
2013-12-19 15:05:38 +0800

    fix bugs

--- apps/frontend/templates/_audio.php
+++ apps/frontend/templates/_audio.php
@@ -1,13 +1,7 @@
-<!-- <audio controls autoplay="autoplay" loop="loop" style="display: none;"> -->
-<!--   <source src="/music/dbz22.mp3" type="audio/mpeg" /> -->
-<!--   <source src="horse.ogg" type="audio/ogg" /> -->
-<!--   <embed height="50" width="100" src="/music/dbz22.mp3" /> -->
-<!-- </audio> -->
-
 <div id="music-box" class="tc" style="position: absolute;top:40px; left:<?php echo $left;?>;">
 	<object type="application/x-shockwave-flash" data="/music/player_mp3_multi.swf" width="200" height="200">
 		<param name="movie" value="/music/player_mp3_multi.swf" />
- 		<param name="FlashVars" value="mp3=/music/dbz.mp3|/music/般若波罗密多心经.mp3|/music/灵山梵音.mp3|/music/禅院钟声.mp3&amp;title=云水蝉心|般若波罗密多心经|灵山梵音|禅院钟声&amp;height=200&amp;autoplay=1&amp;loop=1" />
+ 		<param name="FlashVars" value="mp3=/music/dbz.mp3&amp;title=云水蝉心&amp;height=200&amp;autoplay=1&amp;loop=1" />
 	</object>
 </div>
 
