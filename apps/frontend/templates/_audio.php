8a648e0380deb399c67359fcd1637fb63dd71aed
Luxin YAO <luxin.yao@hotmail.com>
2013-12-22 16:25:24 +0800

    change music

--- apps/frontend/templates/_audio.php
+++ apps/frontend/templates/_audio.php
@@ -1,7 +1,7 @@
 <div id="music-box" class="tc" style="position: absolute;top:40px; left:<?php echo $left;?>;">
 	<object type="application/x-shockwave-flash" data="/music/player_mp3_multi.swf" width="200" height="200">
 		<param name="movie" value="/music/player_mp3_multi.swf" />
- 		<param name="FlashVars" value="mp3=/music/dbz.mp3&amp;title=云水蝉心&amp;height=200&amp;autoplay=1&amp;loop=1" />
+ 		<param name="FlashVars" value="mp3=/music/1.mp3|/music/2.mp3&amp;title=南无大悲观世音|咒&amp;height=200&amp;autoplay=1&amp;loop=1" />
  		<param name="wmode" value="opaque">
 	</object>
 </div>
